8493780e80d9099afb25a7fe23d7ae2f9e14ef49
midrovom <midrovo@callphoneecuador.com>
2025-10-03 16:17:07 -0500

    fix-update

---
+++ theme_prime_image/__manifest__.py
@@ -0,0 +1,18 @@
+{
+    'name': 'Theme Prime Image',
+    'version': '17.0.1.0.0',
+    'summary': 'Extensión visual para theme prime',
+    'category': 'Theme/eCommerce',
+    'depends': ['theme_prime'],
+    'data': [
+        'views/theme_shop_layout_views.xml',
+    ],
+    'assets': {
+        'web.assets_frontend': [
+            #'theme_prime_image/static/src/scss/website_sale_custom.scss',
+        ],
+    },
+    'installable': True,
+    'application': False,
+    'auto_install': False
+}
