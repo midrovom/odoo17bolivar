d8b197628b0521b798a17f7cb46653887dabaaa6
Diego Arcos oficce <darcos@callphoneecuador.com>
2025-10-07 09:26:09 -0500

    fix-update

--- theme_prime_image/static/src/js/s_banner_17.js
+++ theme_prime_image/static/src/js/s_banner_17.js
@@ -1,22 +1,21 @@
 odoo.define('theme_prime_image.s_banner_17', function (require) {
     'use strict';
 
-    const publicWidget = require('web.public.widget');
-
-    publicWidget.registry.Banner17Carousel = publicWidget.Widget.extend({
-        selector: '.s_banner_17',
-        start: function () {
-            const $carousel = this.$el.find('#brandsCarousel');
-
-            // Configuración del carrusel
-            $carousel.carousel({
-                interval: 3000,   // autoplay cada 3s
-                ride: 'carousel',
-                pause: 'hover',
-                wrap: true
+    document.addEventListener('DOMContentLoaded', function () {
+        if (window.innerWidth <= 768) {
+            new Swiper('.swiper-container', {
+                slidesPerView: 5,
+                spaceBetween: 10,
+                loop: true,
+                pagination: {
+                    el: '.swiper-pagination',
+                    clickable: true,
+                },
+                navigation: {
+                    nextEl: '.swiper-button-next',
+                    prevEl: '.swiper-button-prev',
+                },
             });
-
-            return this._super.apply(this, arguments);
-        },
+        }
     });
 });
